9357e5afa3737cda1fb527765b861d028c39b45f
Reed Foster <133254542+Dusk1087@users.noreply.github.com>
2024-12-10 12:55:11 -0500

    updated

--- ch7_practice_importing_data.r
+++ ch7_practice_importing_data.r
@@ -1,3 +1,22 @@
 library(tidyverse)
-students <- read_csv('./data/students.csv', na = c("N/A", ""))
-view(students)
+library(janitor)
+students_raw <- read_csv('./data/students.csv', na = c("N/A",""))
+view(students_raw)
+# students |> 
+#   rename(
+#     student_id = 'Student ID',
+#     full_name = 'Full Name'
+#   )
+students <- students_raw |> 
+  clean_names() 
+
+students
+
+students %>% 
+  distinct(meal_plan)
+
+students <- students |> 
+  mutate(meal_plan = factor(meal_plan))
+
+students |> 
+  distinct(age)

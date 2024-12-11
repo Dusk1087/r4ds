159849db1cd180bfc770fe3615cb19d63d781ae5
Reed Foster <133254542+Dusk1087@users.noreply.github.com>
2024-12-10 20:43:08 -0500

    update from debian

--- ch7_practice_importing_data.r
+++ ch7_practice_importing_data.r
@@ -1,3 +1,28 @@
 library(tidyverse)
 students <- read_csv('./data/students.csv', na = c("N/A", ""))
 view(students)
+
+annoying <- tibble(
+  `1` = 1:10,
+  `2` = `1` * 2 + rnorm(length(`1`))
+)
+view(annoying)
+annoying$`1`
+annoying$`2`
+
+annoying |> 
+  ggplot(aes(x = `1`, y = `2`)) +
+  geom_point() +
+  labs(x = '1', y = '2', title = "Scatterplot")
+
+annoying <- annoying |> 
+  mutate(
+    `3` = `2` / `1`
+  )
+
+annoying |> 
+  rename(
+    one = `1`,
+    two = `2`,
+    three = `3`
+  )

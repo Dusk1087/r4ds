2a3849ef6536a8d0692323a58b3a3215a75fae84
Reed Foster <133254542+Dusk1087@users.noreply.github.com>
2024-12-11 08:24:22 -0500

    updated

--- ch7_practice_importing_data.r
+++ ch7_practice_importing_data.r
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(janitor)
+library(arrow)
 students_raw <- read_csv('./data/students.csv', na = c("N/A",""))
 view(students_raw)
 # students |> 
@@ -7,7 +8,7 @@ view(students_raw)
 #     student_id = 'Student ID',
 #     full_name = 'Full Name'
 #   )
-students <- students_raw |> 
+students_y <- students_raw |> 
   clean_names() 
 
 students
@@ -25,3 +26,21 @@ students |>
   mutate(
     age = parse_number(if_else(age == 'five', '5', age))
   )
+
+file_name <- 'data/new 1.csv'
+view(file_name)
+
+test_file <- list.files("data", pattern = "\\.csv", full.names = TRUE)
+view(test_file)
+
+students
+students_y
+
+write_csv(students_y, "data/students_yy.csv")
+read_csv("students_yy.csv")
+
+write_parquet(students_y, "data/students.parquet")
+read_parquet("data/students.parquet")
+
+write_rds(students_y, "data/students_rds.rds")
+read_rds("data/students_rds.rds")

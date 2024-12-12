5e6c67db72cb091cbad92f40e5aa41ab605c7447
Reed Foster <133254542+Dusk1087@users.noreply.github.com>
2024-12-11 20:01:59 -0500

    updatd:ggplot

--- working with ggplot2.r
+++ working with ggplot2.r
@@ -1,2 +1,21 @@
 library(tidyverse)
 mpg
+mpg |> 
+  ggplot(aes(x = displ, y = hwy, color = class)) +
+  geom_point()
+
+mpg |> 
+  ggplot(aes(x = displ, y = hwy, shape = class)) +
+  geom_point()
+
+mpg |> 
+  ggplot(aes(x = displ, y = hwy, size = class)) +
+  geom_point()
+
+mpg |> 
+  ggplot(aes(x = displ, y = hwy, alpha = class)) +
+  geom_point()
+
+mpg |> 
+  ggplot(aes(x = displ, y = hwy)) +
+  geom_point(color = "blue", shape = 25)

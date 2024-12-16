8790ab8af20f047508fb7b4dd301f9a1b403ffba
Reed Foster <133254542+Dusk1087@users.noreply.github.com>
2024-12-16 10:13:45 -0500

    "updated Debian at dentist"

--- eda.R
+++ eda.R
@@ -1,4 +1,5 @@
 library(tidyverse)
+library(nycflights13)
 ggplot(diamonds, aes(x = carat)) +
   geom_histogram()
 
@@ -11,14 +12,14 @@ ggplot(aes(x = carat)) +
   geom_histogram(binwidth = 0.01)
 
 ggplot(diamonds, aes(x = price)) +
-  geom_histogram(binwidth = .5)
+  geom_histogram(binwidth = 2000)
 
 ggplot(diamonds, aes(x = price)) +
-  geom_histogram(binwidth = .5) +
-  coord_cartesian(ylim = c(0, 40))
+  geom_histogram(binwidth = 10) +
+  coord_cartesian(ylim = c(0, 80))
 
 ggplot(diamonds, aes(x = price)) +
-  geom_histogram(bindwidth = 0.5) +
+  geom_histogram(binwidth = 90) +
   ylim(0, 80)
 
 unusual <- diamonds |> 
@@ -27,4 +28,26 @@ unusual <- diamonds |>
   arrange(y)
 unusual
 
+
+carat_selection <- diamonds |> 
+  filter (carat == 0.99)
+carat_selection
+
+diamonds2 <- diamonds |> 
+  mutate(y = if_else(y < 3 | y > 20, NA, y))
+
+diamonds2 |> 
+  ggplot(aes(x = x, y = y)) +
+  geom_point(na.rm = TRUE)
+
+flights |> 
+  mutate(
+    cancelled = is.na(dep_time),
+    sched_hour = sched_dep_time %/% 100,
+    sched_min = sched_dep_time %% 100,
+    sched_dep_time = sched_hour + (sched_min / 60)
+  ) |> 
+  ggplot(aes(x = sched_dep_time)) +
+  geom_freqpoly(aes(color = cancelled), binwidth = 1/4) +
+  facet_wrap(~ cancelled, scales = "free_y")
 
